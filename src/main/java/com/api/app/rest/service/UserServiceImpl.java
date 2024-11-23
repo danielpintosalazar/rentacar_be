7f494feea61341e1d65d85ae253bc4833944bb11
danielpintosalazar <dactiv2000@gmail.com>
2024-11-23 12:30:44 -0500

    Initial commit: Base configuration Project and Folder Structure with Connection to MySqlDB

---
+++ src/main/java/com/api/app/rest/service/UserServiceImpl.java
@@ -0,0 +1,47 @@
+package com.api.app.rest.service;
+
+import com.api.app.rest.exception.ResourceNotFoundException;
+import com.api.app.rest.model.User;
+import com.api.app.rest.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found with id " + id));
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User updateUser(Long id, User user) {
+        User existingUser = getUserById(id);
+        existingUser.setName(user.getName());
+        existingUser.setEmail(user.getEmail());
+        existingUser.setPassword(user.getPassword());
+        return userRepository.save(existingUser);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
