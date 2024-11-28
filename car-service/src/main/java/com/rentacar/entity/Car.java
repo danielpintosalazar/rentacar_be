f0807bfde65eaa0eec2dbef279f6ae7778ecc178
christian <mesadeayuda@comfacesar.com>
2024-11-28 16:10:26 -0500

    creating model and repository

--- car-service/src/main/java/com/rentacar/entity/Car.java
+++ car-service/src/main/java/com/rentacar/entity/Car.java
@@ -1,5 +1,33 @@
 package com.rentacar.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cars")
 public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    private String nombre;
+
+    @NotEmpty
+    private String marca;
+
+    @NotEmpty
+    private String color;
 
 }
--- car-service/src/main/java/com/rentacar/repository/CarRepository.java
+++ car-service/src/main/java/com/rentacar/repository/CarRepository.java
@@ -1,5 +1,9 @@
 package com.rentacar.repository;
 
-public class CarRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rentacar.entity.Car;
+
+public interface CarRepository extends JpaRepository<Car, Long>{
 
 }
