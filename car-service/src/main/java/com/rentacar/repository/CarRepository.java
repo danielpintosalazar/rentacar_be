9a38a40578e13404e33a15a5bd3f3df135c5c711
christian <mesadeayuda@comfacesar.com>
2024-11-28 17:58:18 -0500

    updating CarService

--- car-service/src/main/java/com/rentacar/repository/CarRepository.java
+++ car-service/src/main/java/com/rentacar/repository/CarRepository.java
@@ -1,9 +1,10 @@
 package com.rentacar.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.rentacar.entity.Car;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long>{
-
 }
--- car-service/src/main/java/com/rentacar/service/CarService.java
+++ car-service/src/main/java/com/rentacar/service/CarService.java
@@ -1,5 +1,48 @@
 package com.rentacar.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rentacar.entity.Car;
+import com.rentacar.repository.CarRepository;
+
+@Service
 public class CarService {
+
+    @Autowired
+    private CarRepository carRepository;
+    
+    public CarService(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
+    public Car getCarbyId(Long id){
+        return carRepository.findById(id).orElse(null);
+    }
+
+    public List<Car> getAllCars(){
+        return carRepository.findAll();
+    }
+
+    public Car addCar(Car car){
+        return carRepository.save(car);
+    }
+
+    public Car updateCar(Long id, Car car){
+        Car existCar = carRepository.findById(id).orElse(null);
+        if (existCar != null){
+            existCar.setNombre(car.getNombre());
+            existCar.setMarca(car.getMarca());
+            existCar.setColor(car.getColor());
+            return carRepository.save(existCar);
+        }
+        return null;
+    }
+
+    public void deleteCar(Long id){
+        carRepository.deleteById(id);
+    }
 
 }
