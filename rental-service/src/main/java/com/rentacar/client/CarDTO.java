57d6b257e6c65a1761d6e181fc1952f2d2dd0185
cybergomez <3838275+cybergomez@users.noreply.github.com>
2024-12-06 21:38:09 -0500

    update cardto and persondto

--- rental-service/src/main/java/com/rentacar/client/CarDTO.java
+++ rental-service/src/main/java/com/rentacar/client/CarDTO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class CarDTO {
     private Long id;
-    private String nombre;
-    private String marca;
+    private String name;
+    private String brand;
     private String color;
 }
--- rental-service/src/main/java/com/rentacar/client/PersonDTO.java
+++ rental-service/src/main/java/com/rentacar/client/PersonDTO.java
@@ -6,4 +6,6 @@ import lombok.Data;
 public class PersonDTO {
     private Long id;
     private String name;
+    private String address;
+    private String phone;
 }
